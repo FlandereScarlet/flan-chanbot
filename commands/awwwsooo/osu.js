de6845148fb1d9696a3b87eaf83554f4b9991392
aguilarralph13 <aguilarralph13@gmail.com>
2016-09-08 20:57:30 -0400

    added experimental command for getting stats
    this uses Nodesu.

---
+++ commands/awwwsooo/osu.js
@@ -0,0 +1,20 @@
+exports.commands = [
+  "osu",
+  "mania",
+  "taiko",
+  "ctb"
+];
+var nodesu = require("nodesu");
+var config = require("./botConfig.json")
+var api = new Nodesu.client(config.osuApiKey);
+
+exports.osu = {
+  name : "osu",
+  desc: "your osu!standard stats",
+  main : function (bot,msg, suffix){
+    var user = suffix.split("");
+    api.user.get(user , Nodesu.Mode.osu).then(data => bot.sendMessage(msg, data));
+  }
+}
+
+  
