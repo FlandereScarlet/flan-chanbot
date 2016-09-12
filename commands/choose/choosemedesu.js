a76eefffb401202b3091fb8b36779520bebeadcd
sr229 <aguilarralph13@gmail.com>
2016-09-12 10:29:04 +0800

    ¯\_(ツ)_/¯

--- commands/choose/choosemedesu.js
+++ commands/choose/choosemedesu.js
@@ -2,11 +2,12 @@ exports.commands = [
   "choose",
   ];
   exports.choose = {
+      //I would smack Recchan if this doesn't work uwu.
     usage : "<option(minimum of 2)>",
     description : "just like decide, only it selects through a set of given parameters.",
     process : function (bot,msg,suffix){
         var textt = message.slice(8);
         var text = textt.split(';')
         var url = text[math.floor(text.length * Math.random())];
-        bot.sendMessage(msg.channel, msg.author, " I choose.. : " `**` + url + `!**`);
+        bot.sendMessage(msg.channel, msg.author, " I choose.. : " `**` + url + `!**` +);
     }
